359fe41ae0470a0b23fe21795ae11b621e98a5a2
Hessam_kk <hessam2koucheki@gmail.com>
2021-11-17 02:00:48 +0330

    add constants for durations

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from win32 import win32gui
-from datetime import datetime
 from time import localtime, sleep
 from os.path import dirname
 from os import makedirs
@@ -7,18 +6,22 @@ from os import makedirs
 TIME = str(localtime().tm_year) +'_'+ str(localtime().tm_mon) +'_'+ str(localtime().tm_mday) +'_'+ str(localtime().tm_hour)
 LOG_FILE_DIR = str(dirname(__file__)) + '\\log\\'
 FILE_ADDR = LOG_FILE_DIR + TIME + '_'
-print(FILE_ADDR)
+
 try:
     f = open(FILE_ADDR + str(localtime().tm_min) + '.txt', 'a')
     f.close()
 except:
     makedirs(LOG_FILE_DIR)
-    
-while 1:
+
+SECOND_TO_CAPRUE = 10 * 60 # 10 MIN
+SECOND_TO_WAIT = 1/2 # 0.5 SECONDs wait before capture next mouse position
+
+for i in range(SECOND_TO_CAPRUE):
     cur_min = localtime().tm_min
     with open(FILE_ADDR + str(cur_min) + '.txt', 'a') as cur_file:
         flags, hcursor, (x,y) = win32gui.GetCursorInfo()
         print(x, y)
         mouse_pos =  ' ' + str(x) + ' ' + str(y)
-        cur_file.write(TIME + '_' +str(localtime().tm_sec) + mouse_pos + '\n')
-        sleep(1)
+        cur_file.write(TIME + '_' + str(localtime().tm_sec) + mouse_pos + '\n')
+        sleep(SECOND_TO_WAIT)
+cur_file.close()
