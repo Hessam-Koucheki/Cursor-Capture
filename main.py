5f000fef4392452fa03a60410bafab43b38aebcc
Hessam_kk <hessam2koucheki@gmail.com>
2021-11-17 02:36:57 +0330

    move mouse as recorded

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from win32 import win32gui
 from time import localtime, sleep
 from os.path import dirname
 from os import makedirs
+import mouse
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
 
 TIME = str(localtime().tm_year) +'_'+ str(localtime().tm_mon) +'_'+ str(localtime().tm_mday) +'_'+ str(localtime().tm_hour)
 LOG_FILE_DIR = str(dirname(__file__)) + '\\log\\'
@@ -13,7 +17,7 @@ try:
 except:
     makedirs(LOG_FILE_DIR)
 
-SECOND_TO_CAPRUE = 10 * 60 # 10 MIN
+SECOND_TO_CAPRUE = 10 # 10 MIN of recording mouse movements
 SECOND_TO_WAIT = 1/2 # 0.5 SECONDs wait before capture next mouse position
 
 for i in range(SECOND_TO_CAPRUE):
@@ -22,6 +26,15 @@ for i in range(SECOND_TO_CAPRUE):
         flags, hcursor, (x,y) = win32gui.GetCursorInfo()
         print(x, y)
         mouse_pos =  ' ' + str(x) + ' ' + str(y)
-        cur_file.write(TIME + '_' + str(localtime().tm_sec) + mouse_pos + '\n')
+        cur_file.write(str(localtime().tm_sec) + mouse_pos + '\n')
         sleep(SECOND_TO_WAIT)
 cur_file.close()
+
+print('Running Mouse Movements')
+with open(FILE_ADDR + str(cur_min) + '.txt', 'r') as cur_file:
+    lines = cur_file.readlines()
+    for line in lines:
+        t, x, y = line.split()
+        print(t, x, y)
+        mouse.move(x,y)
+        sleep(SECOND_TO_WAIT)
