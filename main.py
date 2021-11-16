6e302a45a9c5561732a1cceb63088e65ed1630b1
Hessam_kk <hessam2koucheki@gmail.com>
2021-11-17 01:33:05 +0330

    create a log folder if not exist

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from win32 import win32gui
 from datetime import datetime
 import time
-flags, hcursor, (x,y) = win32gui.GetCursorInfo()
-
-# print(time.localtime().tm_year)
-# print(time.localtime().tm_mon)
-# print(time.localtime().tm_mday)
-# print(time.localtime().tm_hour)
-# print(time.localtime().tm_min)
-
+from os.path import dirname
+from os import makedirs
 
 TIME = str(time.localtime().tm_year) +'_'+ str(time.localtime().tm_mon) +'_'+ str(time.localtime().tm_mday) +'_'+ str(time.localtime().tm_hour) +'_'+ str(time.localtime().tm_min)
-
-FILE_ADDR =  TIME + '.txt'
-
-
-with open(FILE_ADDR, 'w') as cur_file:
+LOG_FILE_DIR = str(dirname(__file__)) + '\\log\\'
+FILE_ADDR = LOG_FILE_DIR + TIME + '.txt'
+print(FILE_ADDR)
+try:
+    open(FILE_ADDR, 'a')
+except:
+    makedirs(LOG_FILE_DIR)
+    
+with open(FILE_ADDR, 'a') as cur_file:
     exit()
 while 1:
     flags, hcursor, (x,y) = win32gui.GetCursorInfo()
     print(x, y)
+
+flags, hcursor, (x,y) = win32gui.GetCursorInfo()
