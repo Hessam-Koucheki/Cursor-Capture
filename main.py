35deb45e00cc23b671ee37f6ae7b9dc78a9a3377
Hessam_kk <hessam2koucheki@gmail.com>
2021-11-17 03:32:57 +0330

    get file name to perform

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from win32 import win32gui
 from time import localtime, sleep
 from os.path import dirname
-from os import makedirs, system
+from os import makedirs, system, listdir, remove
 import mouse
 
 system('cls')
@@ -12,51 +12,79 @@ FILE_ADDR = LOG_FILE_DIR + TIME + '_'
 try:
     f = open(FILE_ADDR + str(localtime().tm_min) + '.txt', 'a')
     f.close()
+    remove(FILE_ADDR + str(localtime().tm_min) + '.txt')
 except:
     makedirs(LOG_FILE_DIR)
 
 SECOND_TO_CAPTURE = 10 # 10 MIN of recording mouse movements
-SECOND_TO_WAIT = 1/2 # 0.5 SECONDs wait before capture next mouse position
+SECOND_TO_WAIT = 1 # 1 SECONDs wait before capture next mouse position
+CLEAR = 'cls'
 
-# Menu
-choice = 123456789
-while choice != 1 and choice != 2:
-    print('Enter 1: Start Mouse Recording.')
-    print('Enter 2: Start Mouse Performing.')
-    choice = int(input('>>> '))
-    if choice != 1 and choice != 2 and choice != 123456789:
-        print('Wrong Input!!', choice, type(choice))
-        sleep(1)
-        system('cls')
-
-# Capturing
-if choice == 1:
-    system('cls')
-    while 1:
-        try:
-            SECOND_TO_CAPTURE = int(input('How many seconds you want to Capture?\n>>> '))
-        except:
-            print('You should enter an integer!!')
+while 1:
+    system(CLEAR)
+    # Menu
+    choice = 123456789
+    while choice != 1 and choice != 2:
+        print('Enter 1: Start Mouse Recording.')
+        print('Enter 2: Start Mouse Performing.')
+        choice = int(input('>>> '))
+        if choice != 1 and choice != 2 and choice != 123456789:
+            print('Wrong Input!!')
             sleep(1)
-            continue
-        break
-    for i in range(SECOND_TO_CAPTURE):
-        cur_min = localtime().tm_min
-        with open(FILE_ADDR + str(cur_min) + '.txt', 'a') as cur_file:
-            flags, hcursor, (x,y) = win32gui.GetCursorInfo()
-            print(x, y)
-            mouse_pos =  ' ' + str(x) + ' ' + str(y)
-            cur_file.write(str(localtime().tm_sec) + mouse_pos + '\n')
-            sleep(1)
-    print('Recording is Finished!!')
+            system(CLEAR)
 
-# Performing
-if choice == 2:
-    print('Running Mouse Movements')
-    with open(FILE_ADDR + str(cur_min) + '.txt', 'r') as cur_file:
-        lines = cur_file.readlines()
-        for line in lines:
-            t, x, y = line.split()
-            print(t, x, y)
-            mouse.move(x,y)
-            sleep(SECOND_TO_WAIT)
+    # Capturing
+    if choice == 1:
+        system(CLEAR)
+        while 1:
+            try:
+                SECOND_TO_CAPTURE = int(input('How many seconds you want to Capture?\n>>> '))
+            except:
+                print('You should enter an integer!!')
+                sleep(1)
+                continue
+            break
+        for i in range(SECOND_TO_CAPTURE):
+            cur_min = localtime().tm_min
+            with open(FILE_ADDR + str(cur_min) + '.txt', 'a') as cur_file:
+                flags, hcursor, (x,y) = win32gui.GetCursorInfo()
+                print(x, y)
+                mouse_pos =  ' ' + str(x) + ' ' + str(y)
+                cur_file.write(str(localtime().tm_sec) + mouse_pos + '\n')
+                sleep(SECOND_TO_WAIT)
+        print('Recording is Finished!!')
+
+    # Performing
+
+    if choice == 2 and listdir(path=LOG_FILE_DIR):
+        while 1:
+            system(CLEAR)
+            try:
+                print('These files are recorded:\n\t', end='')
+                print(*listdir(path=LOG_FILE_DIR), sep='\n\t')
+                cur_file = input('Which file do you want to perform its movements?\n>>> ')
+                if cur_file not in listdir(path=LOG_FILE_DIR):
+                    raise Exception('404')
+            except Exception as exp:
+                if exp.args[0] == '404':
+                    print('File Not Found!! Try Again...')
+                    sleep(1)
+                    continue
+                else:
+                    print('Unkown Error Occured!!')                    
+                    sleep(1)
+                    continue
+            break
+
+        system(CLEAR)
+        print('Running Mouse Movements')
+        with open(FILE_ADDR + str(cur_min) + '.txt', 'r') as cur_file:
+            lines = cur_file.readlines()
+            for line in lines:
+                t, x, y = line.split()
+                print(t, x, y)
+                mouse.move(x,y)
+                sleep(SECOND_TO_WAIT)
+    else:
+        print('No Record history, Start a record first.!')
+        sleep(2)
