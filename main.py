f3b8bebd26fa8cc3dff47f8c9dfe1f42ea9ee799
Hessam_kk <hessam2koucheki@gmail.com>
2021-11-17 03:57:02 +0330

    change record perdion to 1/50

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ except:
     makedirs(LOG_FILE_DIR)
 
 SECOND_TO_CAPTURE = 10 # 10 MIN of recording mouse movements
-SECOND_TO_WAIT = 1 # 1 SECONDs wait before capture next mouse position
+SECOND_TO_WAIT = 1/50 # 1 SECONDs wait before capture next mouse position
 CLEAR = 'cls' # This Used to clear screen, if using Linux use "clear"
 
 # MAIN LOOP
@@ -50,7 +50,7 @@ while 1:
                 continue
             break
         # Started to Capture 
-        for i in range(SECOND_TO_CAPTURE):
+        for i in range(int(SECOND_TO_CAPTURE * (1//SECOND_TO_WAIT))):
             cur_min = localtime().tm_min # store Current minute
             with open(FILE_ADDR + str(cur_min) + '.txt', 'a') as cur_file:
                 flags, hcursor, (x,y) = win32gui.GetCursorInfo()
@@ -59,7 +59,8 @@ while 1:
                 cur_file.write(str(localtime().tm_sec) + mouse_pos + '\n')
                 sleep(SECOND_TO_WAIT)
         print('Recording is Finished!!')
-        
+        sleep(1)
+
     # Exit the program
     if choice == 3:
         print('Have a Happy Day!! :)')
@@ -67,39 +68,40 @@ while 1:
         break
 
     # Performing recorded mouse movement
-    if choice == 2 and listdir(path=LOG_FILE_DIR): # Check we have at least one record
-        # Getting a file name
-        while 1:
+    if choice == 2:
+        if listdir(path=LOG_FILE_DIR): # Check we have at least one record
+            # Getting a file name
+            while 1:
+                system(CLEAR)
+                try:
+                    print('These files are recorded:\n\t', end='')
+                    print(*listdir(path=LOG_FILE_DIR), sep='\n\t')
+                    cur_file = input('Which file do you want to perform its movements?\n>>> ')
+                    if cur_file not in listdir(path=LOG_FILE_DIR):
+                        raise Exception('404')
+                except Exception as exp:
+                    if exp.args[0] == '404':
+                        print('File Not Found!! Try Again...')
+                        sleep(1)
+                        continue
+                    else:
+                        print('Unkown Error Occured!!')                    
+                        sleep(1)
+                        continue
+                break
+            
+            # Start Performing
             system(CLEAR)
-            try:
-                print('These files are recorded:\n\t', end='')
-                print(*listdir(path=LOG_FILE_DIR), sep='\n\t')
-                cur_file = input('Which file do you want to perform its movements?\n>>> ')
-                if cur_file not in listdir(path=LOG_FILE_DIR):
-                    raise Exception('404')
-            except Exception as exp:
-                if exp.args[0] == '404':
-                    print('File Not Found!! Try Again...')
-                    sleep(1)
-                    continue
-                else:
-                    print('Unkown Error Occured!!')                    
-                    sleep(1)
-                    continue
-            break
-        
-        # Start Performing
-        system(CLEAR)
-        print('Running Mouse Movements of ',cur_file)
-        with open(LOG_FILE_DIR + cur_file, 'r') as cur_file:
-            lines = cur_file.readlines()
-            for line in lines:
-                t, x, y = line.split()
-                print(t, x, y)
-                mouse.move(x,y)
-                sleep(SECOND_TO_WAIT)
-        print('Performing is Finished!!')
-        sleep(2)
-    else:
-        print('No Record history, Start a record first.!')
+            print('Running Mouse Movements of ',cur_file)
+            with open(LOG_FILE_DIR + cur_file, 'r') as cur_file:
+                lines = cur_file.readlines()
+                for line in lines:
+                    t, x, y = line.split()
+                    print(t, x, y)
+                    mouse.move(x,y)
+                    sleep(SECOND_TO_WAIT)
+            print('Performing is Finished!!')
+
+        else:
+            print('No Record history, Start a record first.!')
         sleep(2)
